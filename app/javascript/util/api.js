69bbb6aef3aa42aad9097986116c3f390ad2766d
Gary <geehor@gmail.com>
2023-03-08 15:45:52 -0500

    End of a good effort

--- app/javascript/util/api.js
+++ app/javascript/util/api.js
@@ -1,12 +1,27 @@
+const { custom } = require("babel-loader");
+
 const csrfToken = document.querySelector("meta[name=csrf-token]").content;
 
 async function customFetch(url, options = {}) {
   options.headers = {
     // Your code here
-    ...options.headers
+
+    ...options.headers // = key1: val1, key2: val2, key3: val3
   };
 
   return await fetch(url, options);
 }
 
-export const foo = "bar";
+function followUser(id) {
+
+  return customFetch(`/users/${id}/follow`, {
+    method: "POST"
+  });
+}
+
+function unfollowUser(id) {
+
+  return customFetch(`/users/${id}/follow`, {
+    method: "DELETE"
+  });
+}
