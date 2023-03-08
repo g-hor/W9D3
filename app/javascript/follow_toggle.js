4fad886aa2b2dfb2b3f06ada2b3ed9f96d47ca40
“Kelsang” <kelsangrtsering@gmail.com>
2023-03-08 14:15:07 -0500

    work on FollowToggle class

--- app/javascript/follow_toggle.js
+++ app/javascript/follow_toggle.js
@@ -2,11 +2,14 @@ import { API, broadcast } from "./util";
 
 export default class FollowToggle {
   constructor(toggleButton) {
-    // Your code here
+    this.toggleButton = toggleButton;
+    this.handleClick = this.handleClick.bind(this);
+    this.toggleButton.addEventListener("click", this.handleClick);
   }
 
   async handleClick(event) {
-    // Your code here
+    event.preventDefault();
+    console.log(this.followState);
   }
 
   async follow() {
